212b39b1b56cc7bc276c5470a906ea76f74842b2
Jenalyn Cantanero <57343328+jlcantanero@users.noreply.github.com>
2021-05-30 15:38:40 +0800

    Delete esko_app directory

--- esko_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Post, Category, Comment
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Category)
--- esko_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class EskoAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'esko_app'
-
-    def ready(self):
-    	import esko_app.signals
